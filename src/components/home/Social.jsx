cf7976e4ef2fd414b0ffb182b027f60b11654edd
harshshah181205 <harshshah123346@gmail.com>
2025-07-26 14:22:56 +0530

    updated portfolio

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,7 +4,7 @@ const Social = () => {
     return (
         <>
             <div className="home__social">
-                <a href="https://www.instagram.com/harsh.shah1812/" className="home__social-icon" target='_blank'>
+                <a href="https://www.instagram.com/harshhhh.shah/" className="home__social-icon" target='_blank'>
                     <i className='uil uil-instagram'></i>
                 </a>
                 <a href="https://www.linkedin.com/in/harshshah181205/" className="home__social-icon" target='_blank'>
